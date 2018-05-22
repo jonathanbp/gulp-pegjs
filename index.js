6ab7057efa891ac91c9d8dcc5c1a4a72aa42cbf9
Jonathan Bunde-Pedersen <jonathanbp@gmail.com>
2018-05-22 11:02:23 +0200

    Prettier

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-'use strict';
+"use strict";
 
-var gutil     = require('gulp-util');
-var through   = require('through2');
-var assign    = require('object-assign');
+var gutil = require("gulp-util");
+var through = require("through2");
+var assign = require("object-assign");
 
-var pegjs     = require('pegjs');
+var pegjs = require("pegjs");
 
-module.exports = function (opts) {
-	return through.obj(function (file, enc, cb) {
-
+module.exports = function(opts) {
+  return through.obj(function(file, enc, cb) {
     if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+      cb(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-pegjs', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new gutil.PluginError("gulp-pegjs", "Streaming not supported"));
+      return;
+    }
 
     // always generate source code of parser
-		var options = assign({ output: 'source', format: 'commonjs' }, opts);
+    var options = assign({ output: "source", format: "commonjs" }, opts);
 
-		var filePath = file.path;
+    var filePath = file.path;
 
-		try {
-      file.contents = new Buffer(pegjs.generate(file.contents.toString(), options));
-      file.path = gutil.replaceExtension(file.path, '.js');
-			this.push(file);
-		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-pegjs', err, {fileName: filePath}));
-		}
+    try {
+      file.contents = new Buffer(
+        pegjs.generate(file.contents.toString(), options)
+      );
+      file.path = gutil.replaceExtension(file.path, ".js");
+      this.push(file);
+    } catch (err) {
+      this.emit(
+        "error",
+        new gutil.PluginError("gulp-pegjs", err, { fileName: filePath })
+      );
+    }
 
-		cb();
-	});
+    cb();
+  });
 };
